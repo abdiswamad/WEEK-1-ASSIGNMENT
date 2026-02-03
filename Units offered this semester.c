617c6c4bed56027e1669f8268f079cbce1d73ba0
abdiswamad <abdisamadmohamud@students.zetech.ac.ke>
2026-02-03 11:37:27 +0300

    Add files via upload

---
+++ Units offered this semester.c
@@ -0,0 +1,19 @@
+//program to show units offerd this semester
+/*
+Description: program to show units offerd this semester
+Name: ABDISAMAD ALI MOHAMUD
+REG NO:BCS-03-0001/2026
+*/
+#include <stdio.h>//pre-processor directive scanf(), printf()
+//main function
+int main() {
+    printf("Units Offered This Semester:\n");
+    printf("1. Basic Mathematics\n");
+    printf("2. Strucrural Programming And Algorithmsims \n");
+    printf("3. Health And Wellness \n");
+    printf("4. Digital Literacy\n");
+    printf("5. Communication Skills\n");
+    printf("6. Computation Logic\n");
+    printf("7. Introduction to physics\n");
+return 0;
+}
